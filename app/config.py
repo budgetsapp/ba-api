1f058376b8d6e10514bc01d4786948335424aaac
Vadim <korobeinikovv@mail.ru>
2020-01-24 00:19:44 +0300

    Applied flask sqlalchemy

--- app/config.py
+++ app/config.py
@@ -1,18 +1,32 @@
+import os
+
+
 class Config:
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    DATABASE_URL = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
-    DATABASE_URL = 'mysql://user@localhost/db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/db'
 
 
 class DevConfig(Config):
+
+    def __init__(self):
+        self.root = os.path.abspath(os.path.join(os.path.dirname(
+            __file__), '..'))
+
     FLASK_ENV = 'development'
     DEBUG = True
-    DATABASE_URL = 'db_dev/budgetsapp.sqlite'
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        uri = 'sqlite:///{}\\db_dev\\budgetsapp.sqlite'.format(self.root)
+        print(uri)
+        return uri
 
 
 class TestConfig(Config):
--- app/db/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS categories;
-
-CREATE TABLE categories
-(
-    id TEXT PRIMARY KEY,
-    user_id TEXT NOT NULL,
-    name NEXT NOT NULL
-);
