c2c77170ad3d53bec4b86de5c28285cf3d1c35d1
Vadim <korobeinikovv@mail.ru>
2020-01-24 00:19:44 +0300

    Applied flask sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
 
 from flask import Flask
-from .api import categories
+from app.api import categories
+from app import db
+
 
 config = {
     'dev': 'app.config.DevConfig',
@@ -9,13 +11,18 @@ config = {
     'test': 'app.config.TestConfig'
 }
 
+app = Flask(__name__, instance_relative_config=True)
+
 
 def create_app():
     config_name = os.getenv('FLASK_CONFIG', 'dev')
-    app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_object(config[config_name])
-    app.register_blueprint(categories.categories_api)
+    # app.config.from_object(config[config_name])
+    from werkzeug.utils import import_string
+    config_object = import_string(config[config_name])()
+    app.config.from_object(config_object)
+
+    app.register_blueprint(categories.blueprint)
 
     # ensure the db_dev folder exists
     try:
@@ -23,7 +30,6 @@ def create_app():
     except OSError:
         pass
 
-    from .db import setup
-    setup.init_app(app)
+    db.init_app(app)
 
     return app
--- app/api/categories.py
+++ app/api/categories.py
@@ -1,33 +1,33 @@
 from flask import Flask, Blueprint, request, jsonify
 from mocks import categories as categories_mock
 
-categories_api = Blueprint('categories', __name__, url_prefix='/categories')
+blueprint = Blueprint('categories', __name__, url_prefix='/categories')
 
 # url_for('create_category') builds correct path
 
 
-# @categories_api.route('/')
+# @blueprint.route('/')
 # def hello_world():
 #     return 'Hello, World!'
 
 
-@categories_api.route('/new', methods=['POST'])
+@blueprint.route('/new', methods=['POST'])
 def create_category():
     return 'Create category'
 
 # dict will be converted to JSON automatically
 # for other ones use jsonify() or use community extensions
-@categories_api.route('/<uuid:category_id>', methods=['GET'])
+@blueprint.route('/<uuid:category_id>', methods=['GET'])
 def get_category(category_id):
     res = categories_mock.getCategoryById(str(category_id))
     return res
 
 
-@categories_api.route('/<uuid:category_id>', methods=['PATCH'])
+@blueprint.route('/<uuid:category_id>', methods=['PATCH'])
 def update_category(category_id):
     return 'Update category %s' % category_id
 
 
-@categories_api.route('/', methods=['GET'])
+@blueprint.route('/', methods=['GET'])
 def get_categories():
     return jsonify(categories_mock.all_categories)
