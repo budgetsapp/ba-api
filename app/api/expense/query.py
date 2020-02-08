dd9fb1fdca4d0911423c7743b8b92c3b811fc8f6
Vadim <korobeinikovv@mail.ru>
2020-02-08 23:48:35 +0300

    Added expenses total api

--- app/api/expense/query.py
+++ app/api/expense/query.py
@@ -9,6 +9,7 @@ from app.extensions import db
 class Query(graphene.ObjectType):
     my_expenses = graphene.List(
         ExpenseType, first=graphene.Int(), offset=graphene.Int())
+    my_expenses_total = graphene.Int()
 
     @jwt_required
     def resolve_my_expenses(self, info, first, offset):
@@ -16,3 +17,10 @@ class Query(graphene.ObjectType):
         user_id = claims['id']
 
         return db.session.query(ExpenseModel).filter_by(user_id=user_id).offset(offset).limit(first)
+
+    @jwt_required
+    def resolve_my_expenses_total(self, info):
+        claims = get_jwt_claims()
+        user_id = claims['id']
+
+        return db.session.query(ExpenseModel).filter_by(user_id=user_id).count()
